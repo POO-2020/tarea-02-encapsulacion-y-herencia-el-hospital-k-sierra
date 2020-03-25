3df4efeaf5ab01c5dd21e0a331135e4aa531781b
Karla Sierra <ksierra@ucol.mx>
2020-03-24 20:16:36 -0600

    Paciente asegurado terminado

---
+++ pacienteAsegurado.js
@@ -0,0 +1,14 @@
+import Paciente from "./paciente.js"
+export default class PacienteAsegurado extends Paciente{
+    constructor({nombre, fechaNacimiento, telefono, numeroPoliza, fechaFinVigencia, compañia})
+    {
+        super({nombre, fechaNacimiento, telefono})
+        this._numeroPoliza = numeroPoliza
+        this._fechaFinVigencia = fechaFinVigencia
+        this._compañia = compañia
+    }
+    getPerfil()
+    {
+        return (`${this._nombre.getNombreCompleto()}, ${this._fechaNacimiento.getFecha()}, ${this._telefono}, ${this._numeroPoliza}, ${this._fechaFinVigencia.getFecha()}, ${this._compañia}`)
+    }
+}
