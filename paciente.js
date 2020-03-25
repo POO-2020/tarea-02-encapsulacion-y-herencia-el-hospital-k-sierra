0a8b2fed0ca65eea639aaf777e3830fbeddb8797
Karla Sierra <ksierra@ucol.mx>
2020-03-24 19:15:47 -0600

    Paciente terminado

---
+++ paciente.js
@@ -0,0 +1,22 @@
+import Nombre from './nombre.js';
+import Fecha from './fecha.js';
+export default class Pacientes {
+  /**
+   *
+   * @param {Nombre} nombre
+   * @param {Fecha} fechaNacimiento
+   * @param {number} numeroTel
+   */
+  constructor(nombre, fechaNacimiento, telefono) {
+    this._nombre = nombre
+    this._fechaNacimiento = fechaNacimiento
+    this._telefono = telefono
+  }
+  getPerfil() {
+    return `${this._nombre.getNombreCompleto()}, ${this._fechaNacimiento.getFecha()}, ${
+      this._telefono}`
+  }
+  getNombreCompleto(){
+    return this._nombre.getNombreCompleto()
+  }
+}
